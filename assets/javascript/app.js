2c77b8b90ab0ca9f415338f65be0e8e8e3ba893c
rmoj <rmoj99@gmail.com>
2018-07-21 16:39:23 -0400

    create index.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// https://choochooscheduler-307d2.firebaseio.com/
+
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyA9z7yj1LsrEQJnZYjaEHntEXJJk7oBDFw',
+  authDomain: 'choochooscheduler-307d2.firebaseapp.com',
+  databaseURL: 'https://choochooscheduler-307d2.firebaseio.com',
+  projectId: 'choochooscheduler-307d2',
+  storageBucket: 'choochooscheduler-307d2.appspot.com',
+  messagingSenderId: '672866618830'
+};
+firebase.initializeApp(config);
+var db = firebase.database();
