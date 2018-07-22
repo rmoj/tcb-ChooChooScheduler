2eb1f846b7f5a90e63b86234af06989d71426cb7
rmoj <rmoj99@gmail.com>
2018-07-22 15:51:06 -0400

    button click function: add clear form controls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,47 @@
-// https://choochooscheduler-307d2.firebaseio.com/
+$(document).ready(function() {
+  var config = {
+    apiKey: 'AIzaSyA9z7yj1LsrEQJnZYjaEHntEXJJk7oBDFw',
+    authDomain: 'choochooscheduler-307d2.firebaseapp.com',
+    databaseURL: 'https://choochooscheduler-307d2.firebaseio.com',
+    projectId: 'choochooscheduler-307d2',
+    storageBucket: 'choochooscheduler-307d2.appspot.com',
+    messagingSenderId: '672866618830'
+  };
 
-// Initialize Firebase
-var config = {
-  apiKey: 'AIzaSyA9z7yj1LsrEQJnZYjaEHntEXJJk7oBDFw',
-  authDomain: 'choochooscheduler-307d2.firebaseapp.com',
-  databaseURL: 'https://choochooscheduler-307d2.firebaseio.com',
-  projectId: 'choochooscheduler-307d2',
-  storageBucket: 'choochooscheduler-307d2.appspot.com',
-  messagingSenderId: '672866618830'
-};
-firebase.initializeApp(config);
-var db = firebase.database();
+  firebase.initializeApp(config);
+  var db = firebase.database();
 
-$('.btn').on('click', function() {
-  event.preventDefault();
+  $('.btn').on('click', function() {
+    event.preventDefault();
 
-  var name = $('#name')
-    .val()
-    .trim();
-  var destination = $('#destination')
-    .val()
-    .trim();
-  var firstTime = $('#firstTime')
-    .val()
-    .trim();
-  var frequency = $('#frequency')
-    .val()
-    .trim();
+    var name = $('#name')
+      .val()
+      .trim();
+    var destination = $('#destination')
+      .val()
+      .trim();
+    var firstTime = $('#firstTime')
+      .val()
+      .trim();
+    var frequency = $('#frequency')
+      .val()
+      .trim();
 
-  if (
-    name.length > 0 &&
-    destination.length > 0 &&
-    firstTime.length > 0 &&
-    frequency.length > 0
-  ) {
-    db.ref().push({
-      name: name,
-      destination: destination,
-      firstTime: firstTime,
-      frequency: frequency,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
-  }
+    if (
+      name.length > 0 &&
+      destination.length > 0 &&
+      firstTime.length > 0 &&
+      frequency.length > 0
+    ) {
+      db.ref().push({
+        name: name,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+
+      $('.form-control').val('');
+    }
+  });
 });
